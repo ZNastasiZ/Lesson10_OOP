a5541767b3c190504feb50108b33239069fe25f1
Anastasiya Tatarko <fhgjhdgj@gmail.com>
2021-12-12 20:14:43 +0200

    example task

---
+++ src/com/atatarko/Color.java
@@ -0,0 +1,5 @@
+package com.atatarko;
+
+public enum Color {
+    RED, GREEN, YELLOW
+}
---
+++ src/com/atatarko/SelfCheckout.java
@@ -0,0 +1,12 @@
+package com.atatarko;
+
+public class SelfCheckout {
+    public void add(Fruit fruit) {
+        // TODO
+    }
+
+    public double calculatePrice() {
+        return 0;
+    }
+
+}
